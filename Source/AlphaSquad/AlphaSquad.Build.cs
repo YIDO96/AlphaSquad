d76e38c9b89d1e24cb9e658f39fc3344669f713a
VecTor <lsh961101@naver.com>
2024-09-19 18:06:23 +0900

    Initial commit

---
+++ Source/AlphaSquad/AlphaSquad.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class AlphaSquad : ModuleRules
+{
+	public AlphaSquad(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
