e953c7c7d3113454d0f0c4812cbd844e7f78b792
seungok2 <seung1560@gmail.com>
2024-10-07 14:27:24 +0900

    For Git pull Develop
    Collision Testing

---
+++ Source/AlphaSquad/Public/CSW_Enemy_inherit_ForBoss.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CSW_Enemy.h"
+#include "CSW_Enemy_inherit_ForBoss.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ALPHASQUAD_API ACSW_Enemy_inherit_ForBoss : public ACSW_Enemy
+{
+	GENERATED_BODY()
+	
+};
